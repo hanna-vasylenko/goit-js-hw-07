adb38ec5f1a0fd3e084e694e850b6d5c993652fa
Hanna Vasylenko <hanna.vasylenko05@gmail.com>
2022-02-06 15:30:26 +0200

    Adds second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulRef = document.querySelector('.gallery');
+
+const picMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}" />
+      </a>`;
+  })
+  .join('');
+
+ulRef.insertAdjacentHTML('afterbegin', picMarkup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
